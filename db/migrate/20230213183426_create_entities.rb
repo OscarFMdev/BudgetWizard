bea0e10ad98c8f2ae8db9aa8faa31e02896dddd9
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-13 19:43:18 -0600

    Generate migrations

---
+++ db/migrate/20230213183426_create_entities.rb
@@ -0,0 +1,11 @@
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.integer :author_id
+      t.decimal :amount
+
+      t.timestamps
+    end
+  end
+end
