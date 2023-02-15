9bce575cdc5429b975666dea7817957c3a97955f
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-15 14:30:54 -0600

    Add aplication controllers

--- app/controllers/categories_controller.rb
+++
@@ -1,3 +0,0 @@
-class CategoriesController < ApplicationController
-  def index; end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = current_user
+  end
 end
