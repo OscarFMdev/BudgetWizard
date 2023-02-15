890ad9def87754c456774a424d5a48add31460a8
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-15 14:33:11 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root "categories#index"
-  devise_for :users
+  devise_for :users, sign_out_via: [:get, :post]
+  resources :users do
+    resources :groups do
+      resources :entities
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
+  root "groups#index"
 end
